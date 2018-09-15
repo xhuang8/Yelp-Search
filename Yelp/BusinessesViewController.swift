cce139fcd716ab00bb977e45286583bea11e912b
XiaoQian Huang <xiaoqianhuang@sfs-wifi-dhcp-10-143-111-198.wifi.sfsu.edu>
2018-09-14 19:25:21 -0700

    search bar

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UIScrollViewDelegate{
     
     var businesses: [Business]!
+    let searchBar = UISearchBar()
+    var refreshControl: UIRefreshControl!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        
+        //filteredData = data
+        
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
                 self.businesses = businesses
+            self.tableView.reloadData()
                 if let businesses = businesses {
                     for business in businesses {
                         print(business.name!)
@@ -28,16 +41,49 @@ class BusinessesViewController: UIViewController {
             }
         )
         
-        /* Example of Yelp search with more search options specified
-         Business.searchWithTerm(term: "Restaurants", sort: .distance, categories: ["asianfusion", "burgers"]) { (businesses, error) in
-                self.businesses = businesses
-                 for business in self.businesses {
-                     print(business.name!)
-                     print(business.address!)
-                 }
-         }
-         */
+        searchBarControl()
+    }
+   
+    func searchBarControl()
+    {
+        // create the search bar programatically since you won't be
+        // able to drag one onto the navigation bar
+       // searchController.searchBar.sizeToFit()
+         //navigationItem.titleView = searchController.searchBar
+        //searchController.hidesNavigationBarDuringPresentation = false
+        searchBar.showsCancelButton = false
+        searchBar.placeholder = "Search here ..."
+        searchBar.delegate = self
         
+        self.navigationItem.titleView = searchBar
+    }
+    
+    
+    /*func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses  != nil {
+            return businesses!.count
+        }
+        else{
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        
+        cell.business = businesses[indexPath.row]
+        return cell
     }
     
     override func didReceiveMemoryWarning() {
@@ -45,14 +91,6 @@ class BusinessesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
+    
     
 }
